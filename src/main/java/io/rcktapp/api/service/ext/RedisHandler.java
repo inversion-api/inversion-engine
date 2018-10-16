f879291f285765de1bb7359f691461004efd26b3
kfrankic <frankic.dev@gmail.com>
2018-10-16 14:30:34 -0400

    Finished up the RedisHandler
    Added a pool to handle jedis connections.
    
    Sorting request parameters before md5 hashing the url without its
    http/https protocol

--- src/main/java/io/rcktapp/api/service/ext/RedisHandler.java
+++ src/main/java/io/rcktapp/api/service/ext/RedisHandler.java
@@ -1,10 +1,13 @@
 package io.rcktapp.api.service.ext;
 
 import java.time.Duration;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.forty11.j.J;
 import io.forty11.web.js.JS;
 import io.forty11.web.js.JSObject;
 import io.rcktapp.api.Action;
@@ -26,35 +29,29 @@ import redis.clients.jedis.JedisPoolConfig;
  */
 public class RedisHandler implements Handler
 {
-   Logger         log               = LoggerFactory.getLogger(RedisHandler.class);
+   Logger                log               = LoggerFactory.getLogger(RedisHandler.class);
 
-   private String host              = "";
-   private int    port              = 0;
+   private String        host              = "";
+   private int           port              = 0;
 
    // times in milliseconds
-   private int    readSocketTimeout = 0;
-   
-   final JedisPoolConfig poolConfig = buildPoolConfig();
-   JedisPool jedisPool = null;
-   
-   // TODO shut down the pool when closing the app
+   private int           readSocketTimeout = 0;
 
-
-   // TODO should there be a pool of jedis?
-//   private Jedis  jedis             = null;
+   final JedisPoolConfig poolConfig        = buildPoolConfig();
+   JedisPool             jedisPool         = null;
 
    @Override
    public void service(Service service, Api api, Endpoint endpoint, Action action, Chain chain, Request req, Response res) throws Exception
    {
-//      init();
       if (jedisPool == null)
          jedisPool = new JedisPool(poolConfig, host, port, readSocketTimeout);
 
       Jedis jedis = jedisPool.getResource();
       
-      String key = "foo1";
-
-      // TODO verify connection hasnt been dropped
+      // the key is derived from the URL
+      String url = removeUrlProtocol(req.getApiUrl()) + req.getPath() + sortRequestParameters(req.getParams());
+      String key = J.md5(url.getBytes());
+      log.info("url: " + url + " -> key: " + key);
 
       // request should include a json object
       JSObject resJson = null;
@@ -70,7 +67,8 @@ public class RedisHandler implements Handler
       }
       catch (Exception e)
       {
-         // most likely a read socket timeout exception
+         // most likely a read socket timeout exception... log it and move on.
+         log.warn("Failed to retrieve from Redis the key: " + key + " url: " + url, e);
       }
       finally
       {
@@ -88,11 +86,13 @@ public class RedisHandler implements Handler
             // expx EX|PX, expire time units: EX = seconds; PX = milliseconds
             // 106,751,991,167,301 days MAX
             // 15,552,000s == 180 days
-            jedis.set(key, chain.getResponse().getJson().toString(), "NX", "EX", 15552000L);
-            // jedis.setex(key, 12345, req.getJson().toString());
-            
+            // jedis.set(key, chain.getResponse().getJson().toString(), "NX", "EX", 15552000L);
+            // Note: Since the SET command options can replace SETNX, SETEX, PSETEX, it is possible that in future 
+            // versions of Redis these three commands will be deprecated and finally removed.
+            jedis.setex(key, 15552000, chain.getResponse().getJson().toString());
+
             if (jedis != null)
-            jedis.close();
+               jedis.close();
          }
          else
          {
@@ -101,41 +101,66 @@ public class RedisHandler implements Handler
          }
 
       }
-      
-      
 
-      // TODO should a connect/disconnect occur for every transaction or would it be better
-      // to maintain the one connection indefinitely?
-      //jedis.disconnect();
-      //jedis.quit();
+      // TODO close down the pool on shutdown
       //jedisPool.close();
 
    }
 
-//   private void init()
-//   {
-//      if (jedis == null)
-//      {
-//         jedis = new Jedis(host, port, connectionTimeout, readSocketTimeout);
-//         jedis.connect();
-//      }
-//
-//   }
-   
-    
-   private JedisPoolConfig buildPoolConfig() {
-       final JedisPoolConfig poolConfig = new JedisPoolConfig();
-       poolConfig.setMaxTotal(128);
-       poolConfig.setMaxIdle(128);
-       poolConfig.setMinIdle(16);
-       poolConfig.setTestOnBorrow(true);
-       poolConfig.setTestOnReturn(true);
-       poolConfig.setTestWhileIdle(true);
-       poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
-       poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
-       poolConfig.setNumTestsPerEvictionRun(3);
-       poolConfig.setBlockWhenExhausted(true);
-       return poolConfig;
+   /**
+    * Sorts the request parameters alphabetically
+    * @param requestParamMap map representing the request parameters
+    * @return a concatenated string of each param beginning with '?' and joined by '&'
+    */
+   private String sortRequestParameters(Map<String, String> requestParamMap)
+   {
+      TreeMap<String, String> sortedKeyMap = new TreeMap<>(requestParamMap);
+
+      String sortedParams = "";
+
+      boolean isFirstParam = true;
+
+      for (Map.Entry<String, String> entry : sortedKeyMap.entrySet())
+      {
+         if (isFirstParam)
+         {
+            sortedParams += "?";
+            isFirstParam = false;
+         }
+         else
+            sortedParams += "&";
+
+         sortedParams += entry.getKey();
+
+         if (!entry.getValue().isEmpty())
+         {
+            sortedParams += "=" + entry.getValue();
+         }
+
+      }
+
+      return sortedParams;
+   }
+
+   private String removeUrlProtocol(String url)
+   {
+      return url.substring(url.indexOf("://") + 3);
+   }
+
+   private JedisPoolConfig buildPoolConfig()
+   {
+      final JedisPoolConfig poolConfig = new JedisPoolConfig();
+      poolConfig.setMaxTotal(128);
+      poolConfig.setMaxIdle(128);
+      poolConfig.setMinIdle(16);
+      poolConfig.setTestOnBorrow(true);
+      poolConfig.setTestOnReturn(true);
+      poolConfig.setTestWhileIdle(true);
+      poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+      poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+      poolConfig.setNumTestsPerEvictionRun(3);
+      poolConfig.setBlockWhenExhausted(true);
+      return poolConfig;
    }
 
 }
