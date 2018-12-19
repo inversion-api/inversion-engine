41fe003faa590b8604b813a9cb56b356eca30a49
Wells Burke <wells.burke@gmail.com>
2018-12-19 17:48:44 -0500

    improved Parts handling of white space and word boundaries

--- src/test/java/io/rcktapp/rql/TestParts.java
+++ src/test/java/io/rcktapp/rql/TestParts.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
+import io.forty11.j.J;
+
 public class TestParts
 {
 
@@ -25,29 +27,88 @@ public class TestParts
       Parts parts = new Parts(sql);
 
       sql = "SELECT DISTINCT e.dayId, b.receiptTime, var1 AS MobileNumber, var2 AS PointStatus, var3 AS QualifyingItemsInBasket, l.locationcode, l.division, l.region, l.market, p.playercode";
-      assertEquals(sql, parts.select);
+      equals(sql, parts.select);
 
       sql = "FROM event e" + //
             " JOIN Basket b ON e.basketId = b.id AND e.dayId = b.dayId AND e.locationId = b.locationId" + //
             " JOIN LineItem li ON li.basketId = b.id AND li.dayId = b.dayId AND li.locationId = b.locationId" + // 
             " JOIN location l ON  l.id = e.locationid AND l.orgid = e.orgid" + //
             " JOIN player p ON p.locationid = e.locationid AND p.id = e.playerid AND p.orgid = e.orgid";
-      assertEquals(sql, parts.from);
+      equals(sql, parts.from);
 
       sql = "WHERE category = 'LOYALTY' AND event = 'JUUL'" + // 
             " AND   b.voided = false" + //
             " AND   li.voided = false";
-      assertEquals(sql, parts.where);
+      equals(sql, parts.where);
 
       sql = "";
       assertNull(parts.group);
 
       sql = "ORDER BY b.receipttime ASC";
-      assertEquals(sql, parts.order);
+      equals(sql, parts.order);
 
       sql = "LIMIT 50";
-      assertEquals(sql, parts.limit);
+      equals(sql, parts.limit);
 
+   }
+
+   void equals(String str1, String str2)
+   {
+      if (str1 == null || str2 == null)
+      {
+         assertEquals(str1, str2);
+      }
+      else
+      {
+         str1 = str1.replaceAll("\\s+", " ").trim();
+         str2 = str2.replaceAll("\\s+", " ").trim();
+         
+         if (!J.equal(str1, str2))
+            compare(str1, str2);
+
+         assertEquals(str1.trim(), str2.trim());
+      }
+   }
+
+   public static boolean compare(String str1, String str2)
+   {
+      str1 = str1.replaceAll("\\s+", " ").trim();
+      str2 = str2.replaceAll("\\s+", " ").trim();
+
+      if (!str1.equals(str2))
+      {
+         str2 = str2.replaceAll("SQL_CALC_FOUND_ROWS ", "");
+         str2 = str2.replaceAll(" LIMIT 0, 100", "");
+
+         str1 = str1.replaceAll("SQL_CALC_FOUND_ROWS ", "");
+         str1 = str1.replaceAll(" LIMIT 0, 100", "");
+
+         if (!str1.equals(str2))
+         {
+            System.out.println("\r\n");
+            System.out.println("\r\n");
+            System.out.println(str1);
+            System.out.println(str2);
+
+            for (int i = 0; i < str1.length() && i < str2.length(); i++)
+            {
+               if (str1.charAt(i) == str2.charAt(i))
+               {
+                  System.out.print(" ");
+               }
+               else
+               {
+                  System.out.println("X");
+                  break;
+               }
+            }
+            System.out.println(" ");
+
+            String err = "failed test: " + str1 + " != " + str2;
+            return false;
+         }
+      }
+      return true;
    }
 
    @Test
@@ -67,28 +128,28 @@ public class TestParts
       Parts parts = new Parts(sql);
 
       sql = " SELECT DISTINCT e.dayId, b.receiptTime, var1 AS MobileNumber, var2 AS PointStatus, var3 AS QualifyingItemsInBasket, l.locationcode, l.division, l.region, l.market, p.playercode";
-      assertEquals(sql, parts.select);
+      equals(sql.trim(), parts.select);
 
       sql = " FROM event e" + //
             " JOIN Basket b ON e.basketId = b.id AND e.dayId = b.dayId AND e.locationId = b.locationId" + // 
             " JOIN LineItem li ON li.basketId = b.id AND li.dayId = b.dayId AND li.locationId = b.locationId" + // 
             " JOIN location l ON  l.id = e.locationid AND l.orgid = e.orgid" + //
             " JOIN player p ON p.locationid = e.locationid AND p.id = e.playerid AND p.orgid = e.orgid";
-      assertEquals(sql, parts.from);
+      equals(sql, parts.from);
 
       sql = " WHERE category = 'LOYALTY' AND event = 'JUUL'" + //
             " AND   b.voided = false" + //
             " AND   li.voided = false";
-      assertEquals(sql, parts.where);
+      equals(sql, parts.where);
 
       sql = "";
       assertNull(parts.group);
 
       sql = " ORDER BY b.receipttime ASC";
-      assertEquals(sql, parts.order);
+      equals(sql, parts.order);
 
       sql = " LIMIT 50";
-      assertEquals(sql, parts.limit);
+      equals(sql, parts.limit);
 
    }
 
@@ -118,7 +179,7 @@ public class TestParts
       Parts parts = new Parts(sql);
 
       sql = "SELECT DISTINCT e.dayId, b.receiptTime, var1 AS MobileNumber, var2 AS PointStatus, var3 AS QualifyingItemsInBasket, l.locationcode, l.division, l.region, l.market, p.playercode";
-      assertEquals(sql, parts.select);
+      equals(sql, parts.select);
 
       sql = " FROM event e" + //
             "\n" + //
@@ -129,7 +190,7 @@ public class TestParts
             "JOIN location l ON  l.id = e.locationid AND l.orgid = e.orgid\n" + // 
             "\n" + // 
             "JOIN player p ON p.locationid = e.locationid AND p.id = e.playerid AND p.orgid = e.orgid\n";
-      assertEquals(sql, parts.from);
+      equals(sql, parts.from);
 
       sql = "\n" + //
             "WHERE category = 'LOYALTY' AND event = 'JUUL'\n" + // 
@@ -137,18 +198,18 @@ public class TestParts
             "AND   b.voided = false\n" + //
             "\n" + //
             "AND   li.voided = false\n";
-      assertEquals(sql, parts.where);
+      equals(sql, parts.where);
 
       sql = "";
       assertNull(parts.group);
 
       sql = "\n" + //
             "ORDER BY b.receipttime ASC\n";
-      assertEquals(sql, parts.order);
+      equals(sql, parts.order);
 
       sql = "\n" + //
             "LIMIT 50";
-      assertEquals(sql, parts.limit);
+      equals(sql, parts.limit);
 
    }
 
