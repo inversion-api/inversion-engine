59296f5eb1729250f8f98d7bb880279e6018a7d4
wellsb1 <wells.burke@gmail.com>
2019-05-07 21:26:49 -0400

    added test skeleton for testUpsert

--- src/test/java/io/rocketpartners/cloud/action/sql/TestSqlPostAction.java
+++ src/test/java/io/rocketpartners/cloud/action/sql/TestSqlPostAction.java
@@ -1,6 +1,11 @@
 package io.rocketpartners.cloud.action.sql;
 
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +15,9 @@ import org.junit.runners.Parameterized;
 import io.rocketpartners.cloud.model.ObjectNode;
 import io.rocketpartners.cloud.model.Response;
 import io.rocketpartners.cloud.service.Service;
+import io.rocketpartners.cloud.utils.Rows;
+import io.rocketpartners.cloud.utils.Rows.Row;
+import io.rocketpartners.cloud.utils.SqlUtils;
 import junit.framework.TestCase;
 
 @RunWith(Parameterized.class)
@@ -66,6 +74,52 @@ public class TestSqlPostAction extends TestCase
       SqlServiceFactory.prepData(db, url("orders"));
    }
 
+   public void testUpsert() throws Exception
+   {
+      Service service = service();
+      Response res = null;
+
+      SqlDb mysql = (SqlDb) service.getApi("northwind").getDb(db);
+
+      if (mysql.isType("mysql"))
+      {
+         Connection conn = mysql.getConnection();
+         try
+         {
+            Rows rows = SqlUtils.selectRows(conn, "SELECT * FROM Orders WHERE OrderId in(10273,10274,10275,10276)");
+
+            for (Row row : rows)
+            {
+               row.put("shipaddress", "testing_upsert");
+            }
+
+            Map clone1 = new HashMap(rows.get(0));
+            clone1.remove("OrderId");
+
+            Map clone2 = new HashMap(rows.get(0));
+            clone1.put("OrderId", 1);
+
+            rows.addRow(clone1);
+            rows.addRow(clone2);
+
+            List<Map<String, Object>> toUpsert = new ArrayList(rows);
+
+            List generatedKeys = SqlUtils.mysqlUpsert(conn, "Order", toUpsert);
+
+            //start test here
+            //the generated key value for orders 10273,10274,10275 and 10276 is possibly null
+            //clone1 should have a generated key
+            //clone2 may not have a generated key.
+         }
+         finally
+         {
+            conn.close();
+         }
+
+      }
+
+   }
+
    @Test
    public void testAddOneRecord() throws Exception
    {
@@ -92,67 +146,66 @@ public class TestSqlPostAction extends TestCase
       assertEquals(26, res.find("meta.foundRows"));
       assertEquals(res.find("data.0.href"), url("orders/222000"));
    }
-   
-   
-//   public void testAddUpdateMultipleRecords() throws Exception
-//   {
-//      //some of the selected records are already in the target db (from the pre test config)
-//      //and some are not.  This will cause some records to insert and some to update.
-//      res = service.get("http://localhost/northwind/source/orders?limit=25&sort=orderid&page=2&excludes=href");
-//      assertEquals(10273, res.find("data.0.orderid"));
-//      assertEquals(10297, res.find("data.24.orderid"));
-//
-//      //now dump what we just selected into the new db (some will insert some will update)
-//      String putVal = res.data().toString();
-//      res = service.post(url("orders"), putVal);
-//      assertTrue(res.findString("data.0.href").endsWith("10273"));
-//      assertTrue(res.findString("data.24.href").endsWith("10297"));
-//
-//      String location = res.getHeader("Location");
-//
-//      assertEquals(url("orders/10273,10274,10275,10276,10277,10278,10279,10280,10281,10282,10283,10284,10285,10286,10287,10288,10289,10290,10291,10292,10293,10294,10295,10296,10297"), //
-//            location);
-//
-//      assertEquals(51, service.get(url("orders?limit=1")).find("meta.foundRows"));
-//
-//      //correcting for the differencees in the hrefs, we should be able to get the inserted
-//      //rows from the source and dest and they should match
-//      String srcLocation = location.replace("/" + db + "/", "/source/");
-//      String src = service.get(srcLocation).data().toString();
-//      String copy = service.get(location).data().toString();
-//      src = src.replace("/source/", "/" + db + "/");
-//      assertEquals(src, copy);
-//
-//      //we did not insert any order details
-//      res = service.get("http://localhost/northwind/h2/orderdetails");
-//      res.dump();
-//      assertEquals(0, res.findInt("meta.foundRows"));
-//
-//      //now we are going to reselect the copied rows and expand the orderdetails relationship
-//      //and then post those
-//      srcLocation += "?expands=orderdetails&excludes=orderdetails.href,orderdetails.employees";
-//
-//      res = service.get(srcLocation);
-//      assertNull(res.findString("data.0.orderdetails.0.href"));
-//      assertNull(res.findString("data.0.orderdetails.0.employees"));
-//      assertTrue(res.findString("data.0.orderdetails.0.order").endsWith("/orders/10273"));
-//      assertTrue(res.findString("data.0.orderdetails.0.product").endsWith("/products/10"));
-//
-//      putVal = res.data().toString().replace("/source/", "/h2/");
-//
-//      res = service.post(url("orders"), putVal);
-//      location = res.getHeader("Location");
-//
-//      assertEquals(51, service.get(url("orders?limit=1")).find("meta.foundRows"));
-//
-//      res = service.get(url("orderdetails"));
-//      res.dump();
-//      assertEquals(0, res.findInt("meta.foundRows"));
-//
-//      res.dump();
-//
-//      //    res = service.get("http://localhost/northwind/source/orders?limit=25&sort=orderid&page=4&excludes=href,orderid,shipname,orderdetails,customer,employee,shipvia");
-//
-//   }
+
+   //   public void testAddUpdateMultipleRecords() throws Exception
+   //   {
+   //      //some of the selected records are already in the target db (from the pre test config)
+   //      //and some are not.  This will cause some records to insert and some to update.
+   //      res = service.get("http://localhost/northwind/source/orders?limit=25&sort=orderid&page=2&excludes=href");
+   //      assertEquals(10273, res.find("data.0.orderid"));
+   //      assertEquals(10297, res.find("data.24.orderid"));
+   //
+   //      //now dump what we just selected into the new db (some will insert some will update)
+   //      String putVal = res.data().toString();
+   //      res = service.post(url("orders"), putVal);
+   //      assertTrue(res.findString("data.0.href").endsWith("10273"));
+   //      assertTrue(res.findString("data.24.href").endsWith("10297"));
+   //
+   //      String location = res.getHeader("Location");
+   //
+   //      assertEquals(url("orders/10273,10274,10275,10276,10277,10278,10279,10280,10281,10282,10283,10284,10285,10286,10287,10288,10289,10290,10291,10292,10293,10294,10295,10296,10297"), //
+   //            location);
+   //
+   //      assertEquals(51, service.get(url("orders?limit=1")).find("meta.foundRows"));
+   //
+   //      //correcting for the differencees in the hrefs, we should be able to get the inserted
+   //      //rows from the source and dest and they should match
+   //      String srcLocation = location.replace("/" + db + "/", "/source/");
+   //      String src = service.get(srcLocation).data().toString();
+   //      String copy = service.get(location).data().toString();
+   //      src = src.replace("/source/", "/" + db + "/");
+   //      assertEquals(src, copy);
+   //
+   //      //we did not insert any order details
+   //      res = service.get("http://localhost/northwind/h2/orderdetails");
+   //      res.dump();
+   //      assertEquals(0, res.findInt("meta.foundRows"));
+   //
+   //      //now we are going to reselect the copied rows and expand the orderdetails relationship
+   //      //and then post those
+   //      srcLocation += "?expands=orderdetails&excludes=orderdetails.href,orderdetails.employees";
+   //
+   //      res = service.get(srcLocation);
+   //      assertNull(res.findString("data.0.orderdetails.0.href"));
+   //      assertNull(res.findString("data.0.orderdetails.0.employees"));
+   //      assertTrue(res.findString("data.0.orderdetails.0.order").endsWith("/orders/10273"));
+   //      assertTrue(res.findString("data.0.orderdetails.0.product").endsWith("/products/10"));
+   //
+   //      putVal = res.data().toString().replace("/source/", "/h2/");
+   //
+   //      res = service.post(url("orders"), putVal);
+   //      location = res.getHeader("Location");
+   //
+   //      assertEquals(51, service.get(url("orders?limit=1")).find("meta.foundRows"));
+   //
+   //      res = service.get(url("orderdetails"));
+   //      res.dump();
+   //      assertEquals(0, res.findInt("meta.foundRows"));
+   //
+   //      res.dump();
+   //
+   //      //    res = service.get("http://localhost/northwind/source/orders?limit=25&sort=orderid&page=4&excludes=href,orderid,shipname,orderdetails,customer,employee,shipvia");
+   //
+   //   }
 
 }
