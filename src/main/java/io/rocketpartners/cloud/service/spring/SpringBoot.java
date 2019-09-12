bec0bf8282104c36bf9240b1d8cc3bad4f6eb340
wellsb1 <wells.burke@gmail.com>
2019-09-12 12:40:32 -0400

    Added some helpful debug for SB startup errors

--- src/main/java/io/rocketpartners/cloud/service/spring/SpringBoot.java
+++ src/main/java/io/rocketpartners/cloud/service/spring/SpringBoot.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 import io.rocketpartners.cloud.service.Service;
 import io.rocketpartners.cloud.service.Servlet;
+import io.rocketpartners.cloud.utils.Utils;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
@@ -19,8 +20,38 @@ public class SpringBoot
 
    public static void run(Service inService)
    {
-      service = inService;
-      SpringApplication.run(SpringBoot.class);
+      try
+      {
+         service = inService;
+         SpringApplication.run(SpringBoot.class);
+      }
+      catch (Throwable e)
+      {
+         e = Utils.getCause(e);
+         if (Utils.getStackTraceString(e).indexOf("A child container failed during start") > -1)
+         {
+            String msg = "";
+            msg = " README FOR HELP!!!!!!!";
+            msg += "\n";
+            msg += "\n It looks like you are getting a frustrating Tomcat startup error.";
+            msg += "\n";
+            msg += "\n This error may be casused if URL.setURLStreamHandlerFactory()";
+            msg += "\n is somehow called before Spring Boot starts Tomcat. ";
+            msg += "\n";
+            msg += "\n This seems to be a frustrating undocumented \"no no\" of Tomcat with ";
+            msg += "\n Spring Boot. Using H2 db before Spring Boot starts Tomcat seems to ";
+            msg += "\n be one known cause of this error.";
+            msg += "\n";
+            msg += "\n SOLUTION: Override Service.startup0() and place all of your Api wiring";
+            msg += "\n and other setup code there.  That way Tomcat will load before ";
+            msg += "\n the part of your code that is causing this unintended side effect.";
+            msg += "\n\n\n";
+
+            System.err.println(msg);
+            throw new RuntimeException(msg, e);
+         }
+         Utils.rethrow(e);
+      }
    }
 
    @Bean
