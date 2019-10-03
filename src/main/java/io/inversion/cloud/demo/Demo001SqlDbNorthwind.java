4da32a3b250732e98a2f7698e36107ad5398d19a
wellsb1 <wells.burke@gmail.com>
2019-10-03 12:21:35 -0400

    inversion.io refactors

--- src/main/java/io/inversion/cloud/demo/Demo001SqlDbNorthwind.java
+++ src/main/java/io/inversion/cloud/demo/Demo001SqlDbNorthwind.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2015-2019 Inversion.org, LLC
- * http://inversion.io
+ * https://github.com/inversion-api
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
