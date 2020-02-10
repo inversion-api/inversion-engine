be0c3f26bfb7036702f106310507b204d0caaf82
wellsb1 <wells.burke@gmail.com>
2020-02-10 14:19:26 -0500

    tweak to #59 Added logging to the failure of JWT verification

--- inversion-api/src/main/java/io/inversion/cloud/action/security/AuthAction.java
+++ inversion-api/src/main/java/io/inversion/cloud/action/security/AuthAction.java
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,9 +18,7 @@ package io.inversion.cloud.action.security;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
-import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -37,22 +35,15 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
-import io.inversion.cloud.action.sql.SqlDb;
 import io.inversion.cloud.model.Action;
-import io.inversion.cloud.model.Api;
 import io.inversion.cloud.model.ApiException;
-import io.inversion.cloud.model.Endpoint;
 import io.inversion.cloud.model.JSArray;
 import io.inversion.cloud.model.JSNode;
 import io.inversion.cloud.model.Request;
 import io.inversion.cloud.model.Response;
-import io.inversion.cloud.model.SC;
+import io.inversion.cloud.model.Status;
 import io.inversion.cloud.model.User;
 import io.inversion.cloud.service.Chain;
-import io.inversion.cloud.service.Engine;
-import io.inversion.cloud.utils.Rows;
-import io.inversion.cloud.utils.Rows.Row;
-import io.inversion.cloud.utils.SqlUtils;
 import io.inversion.cloud.utils.Utils;
 
 public class AuthAction extends Action<AuthAction>
@@ -82,7 +73,7 @@ public class AuthAction extends Action<AuthAction>
    }
 
    @Override
-   public void run(Engine engine, Api api, Endpoint endpoint, Chain chain, Request req, Response resp) throws Exception
+   public void run(Request req, Response resp) throws Exception
    {
       //one time init
       if (sessionCache == null)
@@ -158,14 +149,16 @@ public class AuthAction extends Action<AuthAction>
                }
                catch (Exception ex)
                {
-                  // TODO Why not just throw the 401 here?  With a context specific log?
-                  log.debug("Attempting to validate token failed {}  Continuing.", token, ex) ;
+                  //-- this is not an error yet because there can be multiple signing keys in the list
+                  //-- and this jwt may be using an older/different but still supported one in the list
+                  //--
+                  //-- multiple keys are supported so you can release keys with new signatures
+                  //-- while supporting old signatures for some transition period.
                }
             }
 
-            // The only reason this will be true is if there are no getJwtSecrets() or an Exception above
             if (jwt == null)
-               throw new ApiException(SC.SC_401_UNAUTHORIZED);
+               throw new ApiException(Status.SC_401_UNAUTHORIZED);
 
             user = createUserFromValidJwt(jwt);
          }
@@ -207,7 +200,7 @@ public class AuthAction extends Action<AuthAction>
                sessionKey = url.substring(url.lastIndexOf("/") + 1, url.length());
 
             if (sessionKey == null)
-               throw new ApiException(SC.SC_400_BAD_REQUEST, "Logout requires a session authroization or x-auth-token header");
+               throw new ApiException(Status.SC_400_BAD_REQUEST, "Logout requires a session authroization or x-auth-token header");
 
             sessionCache.remove(sessionKey);
          }
@@ -217,7 +210,7 @@ public class AuthAction extends Action<AuthAction>
             if (salt == null)
             {
                log.warn("You must configure a salt value for password hashing.");
-               throw new ApiException(SC.SC_500_INTERNAL_SERVER_ERROR);
+               throw new ApiException(Status.SC_500_INTERNAL_SERVER_ERROR);
             }
 
             user = dao.getUser(username, accountCode, apiCode, tenantCode);
@@ -288,7 +281,7 @@ public class AuthAction extends Action<AuthAction>
             }
 
             if (user == null)
-               throw new ApiException(SC.SC_401_UNAUTHORIZED);
+               throw new ApiException(Status.SC_401_UNAUTHORIZED);
          }
       }
 
@@ -340,7 +333,7 @@ public class AuthAction extends Action<AuthAction>
       {
          User loggedIn = Chain.getUser();
          if (api.isMultiTenant() && (req.getTenantCode() == null || !req.getTenantCode().equalsIgnoreCase(loggedIn.getTenantCode())))
-            throw new ApiException(SC.SC_401_UNAUTHORIZED);
+            throw new ApiException(Status.SC_401_UNAUTHORIZED);
       }
 
       if (user == null && !sessionReq)
@@ -422,7 +415,7 @@ public class AuthAction extends Action<AuthAction>
          user.withPermissions(perms.toArray(new String[perms.size()]));
       }
    }
-   
+
    /**
     * Looks gwt signing secrets up as environment vars or sysprops.
     * 
@@ -451,7 +444,7 @@ public class AuthAction extends Action<AuthAction>
 
          for (int j = 2; j >= 0; j--)
          {
-            String key = (getName() != null ? getName() : "") + ".jwt" + (i == 0 ? "" : ("." + i));
+            String key = (getCollectionName() != null ? getCollectionName() : "") + ".jwt" + (i == 0 ? "" : ("." + i));
 
             if (j > 0 && accountCode != null)
                key += "." + accountCode;
@@ -464,7 +457,7 @@ public class AuthAction extends Action<AuthAction>
 
             key += ".secret";
 
-            String secret = Utils.findSysEnvPropStr(key, null);
+            String secret = Utils.getSysEnvPropStr(key, null);
             if (secret != null)
             {
                secrets.add(secret);
@@ -511,7 +504,7 @@ public class AuthAction extends Action<AuthAction>
       }
       catch (Exception ex)
       {
-         throw new ApiException(SC.SC_500_INTERNAL_SERVER_ERROR);
+         throw new ApiException(Status.SC_500_INTERNAL_SERVER_ERROR);
       }
    }
 
@@ -595,15 +588,15 @@ public class AuthAction extends Action<AuthAction>
       return this;
    }
 
-   public SqlDbUserDao withSalt(String salt)
+   public AuthAction withSalt(String salt)
    {
       this.salt = salt;
-      return null;
+      return this;
    }
 
    public String getSalt()
    {
-      return Utils.findSysEnvPropStr(getName() + ".salt", salt);
+      return Utils.getSysEnvPropStr(getCollectionName() + ".salt", salt);
    }
 
    class LRUAuthSessionCache implements AuthSessionCache
@@ -639,224 +632,6 @@ public class AuthAction extends Action<AuthAction>
    public static interface UserDao
    {
       User getUser(String username, String accountCode, String apiCode, String tenantCode) throws Exception;
-   }
-
-   public static class SqlDbUserDao implements UserDao
-   {
-      SqlDb db = null;
-
-      public SqlDbUserDao()
-      {
-
-      }
-
-      public SqlDbUserDao(SqlDb db)
-      {
-         withDb(db);
-      }
-
-      public SqlDbUserDao withDb(SqlDb db)
-      {
-         this.db = db;
-         return this;
-      }
-
-      public SqlDb getDb()
-      {
-         return db;
-      }
-
-      public User getUser(String username, String accountCode, String apiCode, String tenantCode) throws Exception
-      {
-         Connection conn = null;
-         User user = null;
-         try
-         {
-            List params = new ArrayList();
-            String sql = "";
-
-            if (username != null)
-            {
-               params.add(username);
-               sql += " SELECT DISTINCT u.*";
-               sql += " FROM User u   ";
-               sql += " WHERE (u.revoked IS NULL OR u.revoked != 1) ";
-               sql += " AND u.username = ? ";
-               //sql += " AND (u.password = ? OR u.password = ?)";
-               sql += " LIMIT 1 ";
-            }
-            //            else
-            //            {
-            //               params.add(apiCode);
-            //               sql += " SELECT DISTINCT u.*";
-            //               sql += " FROM User u   ";
-            //               sql += " JOIN ApiKey a ON a.userId = u.id";
-            //               sql += " WHERE (u.revoked IS NULL OR u.revoked != 1) ";
-            //               sql += " AND (a.revoked IS NULL OR a.revoked != 1) ";
-            //               sql += " AND a.accessKey = ? ";
-            //               sql += " AND (a.secretKey = ? OR a.secretKey = ?)";
-            //               sql += " LIMIT 1 ";
-            //            }
-
-            conn = db.getConnection();
-            user = SqlUtils.selectObject(conn, sql, User.class, username);
-
-            if (user != null)
-            {
-               Rows rows = findGRP(conn, user.getId(), accountCode, apiCode, tenantCode);
-               if (rows == null)
-               {
-                  //-- there is a users with the given username but the don't have any association to this accountCoce/apiCode/tenantCode
-                  user = null;
-               }
-               else
-               {
-                  populateGRP(user, rows);
-               }
-            }
-         }
-         finally
-         {
-            conn.close();
-         }
-
-         return user;
-      }
-
-      void populateGRP(User user, Rows rows)
-      {
-         for (Row row : rows)
-         {
-            String type = row.getString("type");
-            String name = row.getString("name");
-            if (name != null)
-            {
-               switch (type)
-               {
-                  case "group":
-                     user.withGroups(name);
-                     break;
-                  case "role":
-                     user.withRoles(name);
-                     break;
-                  case "permission":
-                     user.withPermissions(name);
-                     break;
-               }
-            }
-         }
-      }
-
-      /**
-       * user -> permission
-       * user -> group -> permission
-       * user -> role -> permission
-       * user -> group -> role -> permission
-      
-       * UserPermission
-       * GroupPermission
-       * RolePermission
-       * UserGroup
-       * UserRole
-       * GroupRole
-       * 
-       * 
-       * 
-       */
-      Rows findGRP(Connection conn, int userId, String accountCode, String apiCode, String tenantCode) throws Exception
-      {
-         List vals = new ArrayList();
-
-         String sql = "";
-
-         sql += "SELECT * FROM (";
-
-         //-- user -> permission
-         sql += "\r\n    SELECT 'permission' as type, p.name, 'user->permission' as via";
-         sql += "\r\n    FROM Permission p";
-         sql += "\r\n    JOIN UserPermission u ON p.id = u.permissionId";
-         sql += "\r\n    WHERE u.userId = ?";
-         vals.add(userId);
-
-         sql += "\r\n     AND ((p.accountCode is null OR p.accountCode = ? ) AND (p.apiCode is null OR p.apiCode = ?) AND (p.tenantCode is null OR p.tenantCode = ?))";
-         sql += "\r\n     AND ((u.accountCode is null OR u.accountCode = ? ) AND (u.apiCode is null OR u.apiCode = ?) AND (u.tenantCode is null OR u.tenantCode = ?))";
-         vals.addAll(Arrays.asList(accountCode, apiCode, tenantCode, accountCode, apiCode, tenantCode));
-
-         //-- user -> group -> permission
-         sql += "\r\n                                                           ";
-         sql += "\r\n    UNION";
-         sql += "\r\n                                                           ";
-         sql += "\r\n    SELECT 'permission' as type, p.name, 'user->group->permission' as via";
-         sql += "\r\n    FROM Permission p";
-         sql += "\r\n    JOIN GroupPermission g ON p.id = g.permissionId";
-         sql += "\r\n    JOIN UserGroup u ON u.groupId = g.groupId ";
-         sql += "\r\n    WHERE u.userId = ?";
-         sql += "\r\n     AND ((p.accountCode is null OR p.accountCode = ? ) AND (p.apiCode is null OR p.apiCode = ?) AND (p.tenantCode is null OR p.tenantCode = ?))";
-         sql += "\r\n     AND ((g.accountCode is null OR g.accountCode = ? ) AND (g.apiCode is null OR g.apiCode = ?) AND (g.tenantCode is null OR g.tenantCode = ?))";
-         sql += "\r\n     AND ((u.accountCode is null OR u.accountCode = ? ) AND (u.apiCode is null OR u.apiCode = ?) AND (u.tenantCode is null OR u.tenantCode = ?))";
-
-         vals.addAll(Arrays.asList(userId, accountCode, apiCode, tenantCode, accountCode, apiCode, tenantCode, accountCode, apiCode, tenantCode));
-
-         //-- user -> role -> permission
-         sql += "\r\n                                                           ";
-         sql += "\r\n    UNION";
-         sql += "\r\n                                                           ";
-         sql += "\r\n    SELECT 'permission' as type, p.name, 'user->role->permission' as via";
-         sql += "\r\n    FROM Permission p";
-         sql += "\r\n    JOIN RolePermission r ON p.id = r.permissionId";
-         sql += "\r\n    JOIN UserRole u ON u.roleId = r.roleId ";
-         sql += "\r\n    WHERE u.userId = ?";
-         sql += "\r\n     AND ((p.accountCode is null OR p.accountCode = ? ) AND (p.apiCode is null OR p.apiCode = ?) AND (p.tenantCode is null OR p.tenantCode = ?))";
-         sql += "\r\n     AND ((r.accountCode is null OR r.accountCode = ? ) AND (r.apiCode is null OR r.apiCode = ?) AND (r.tenantCode is null OR r.tenantCode = ?))";
-         sql += "\r\n     AND ((u.accountCode is null OR u.accountCode = ? ) AND (u.apiCode is null OR u.apiCode = ?) AND (u.tenantCode is null OR u.tenantCode = ?))";
-
-         vals.addAll(Arrays.asList(userId, accountCode, apiCode, tenantCode, accountCode, apiCode, tenantCode, accountCode, apiCode, tenantCode));
-
-         //-- user -> group -> role -> permission
-         sql += "\r\n                                                           ";
-         sql += "\r\n    UNION";
-         sql += "\r\n                                                           ";
-         sql += "\r\n    SELECT 'permission' as type, p.name, 'user->group->role->permission' as via";
-         sql += "\r\n    FROM Permission p";
-         sql += "\r\n    JOIN RolePermission r ON p.id = r.permissionId";
-         sql += "\r\n    JOIN GroupRole g ON r.roleID = g.roleId";
-         sql += "\r\n    JOIN UserGroup u ON g.groupId = u.groupId";
-         sql += "\r\n    WHERE u.userId = ?";
-
-         sql += "\r\n     AND ((p.accountCode is null OR p.accountCode = ? ) AND (p.apiCode is null OR p.apiCode = ?) AND (p.tenantCode is null OR p.tenantCode = ?))";
-         sql += "\r\n     AND ((r.accountCode is null OR r.accountCode = ? ) AND (r.apiCode is null OR r.apiCode = ?) AND (r.tenantCode is null OR r.tenantCode = ?))";
-         sql += "\r\n     AND ((g.accountCode is null OR g.accountCode = ? ) AND (g.apiCode is null OR g.apiCode = ?) AND (g.tenantCode is null OR g.tenantCode = ?))";
-         sql += "\r\n     AND ((u.accountCode is null OR u.accountCode = ? ) AND (u.apiCode is null OR u.apiCode = ?) AND (u.tenantCode is null OR u.tenantCode = ?))";
-
-         vals.addAll(Arrays.asList(userId, accountCode, apiCode, tenantCode, accountCode, apiCode, tenantCode, accountCode, apiCode, tenantCode, accountCode, apiCode, tenantCode));
-
-         //-- user -> group
-         sql += "\r\n                                                           ";
-         sql += "\r\n    UNION";
-         sql += "\r\n                                                           ";
-         sql += "\r\n    SELECT 'group' as type, g.name, '' as via";
-         sql += "\r\n    FROM `Group` g";
-         sql += "\r\n    JOIN UserGroup u ON g.id = u.groupId";
-         sql += "\r\n    WHERE u.userId = ?";
-         sql += "\r\n     AND ((u.accountCode is null OR u.accountCode = ? ) AND (u.apiCode is null OR u.apiCode = ?) AND (u.tenantCode is null OR u.tenantCode = ?))";
-         vals.addAll(Arrays.asList(userId, accountCode, apiCode, tenantCode));
-
-         //-- user -> role
-         sql += "\r\n                                                           ";
-         sql += "\r\n    UNION";
-         sql += "\r\n                                                           ";
-         sql += "\r\n    SELECT 'role' as type, r.name, '' as via";
-         sql += "\r\n    FROM Role r";
-         sql += "\r\n    JOIN UserRole u ON r.id = u.roleId";
-         sql += "\r\n    WHERE u.userId = ?";
-         sql += "\r\n     AND ((u.accountCode is null OR u.accountCode = ? ) AND (u.apiCode is null OR u.apiCode = ?) AND (u.tenantCode is null OR u.tenantCode = ?))";
-         vals.addAll(Arrays.asList(userId, accountCode, apiCode, tenantCode));
-
-         sql += " ) as q ORDER BY type, name, via";
-
-         return SqlUtils.selectRows(conn, sql, vals);
-      }
-
    }
 
 }
