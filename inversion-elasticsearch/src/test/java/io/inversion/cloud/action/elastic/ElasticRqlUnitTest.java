1f4d8698a6e6a47b871839893ae6c9f77f8cdc99
kfrankic <kevin@rocketpartners.io>
2020-05-06 16:05:22 -0400

    Supported tests for the suite

--- inversion-elasticsearch/src/test/java/io/inversion/cloud/action/elastic/ElasticRqlUnitTest.java
+++ inversion-elasticsearch/src/test/java/io/inversion/cloud/action/elastic/ElasticRqlUnitTest.java
@@ -33,62 +33,24 @@ public class ElasticRqlUnitTest extends AbstractRqlTest
 
       suite//
            .withResult("eq", "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"filter\" : [ { \"term\" : { \"orderID\" : { \"value\" : \"10248\", \"boost\" : 1.0 } } }, { \"term\" : { \"shipCountry\" : { \"value\" : \"France\", \"boost\" : 1.0 } } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
-           //           .withResult("ne", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE (NOT (orders[\\\"shipCountry\\\"] = @shipCountry1)) ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCountry1\",\"value\":\"France\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("n", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE IS_NULL (orders[\\\"shipRegion\\\"]) ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("nn", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE orders[\\\"shipRegion\\\"] <> null ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("emp", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE (orders[\\\"shipRegion\\\"] IS NULL OR orders[\\\"shipRegion\\\"] = '') ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("nemp", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE (orders[\\\"shipRegion\\\"] IS NOT NULL AND orders[\\\"shipRegion\\\"] != '') ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("sw", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE STARTSWITH (orders[\\\"shipCountry\\\"], @shipCountry1) ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCountry1\",\"value\":\"Franc\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("ew", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE ENDSWITH (orders[\\\"shipCountry\\\"], @shipCountry1) ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCountry1\",\"value\":\"nce\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("w", "CosmosDb supports 'sw' and 'ew' but not 'w' or 'wo' functions.")//
-           //           .withResult("wo", "CosmosDb supports 'sw' and 'ew' but not 'w' or 'wo' functions.")//
-           //           .withResult("lt", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE orders[\\\"freight\\\"] < @freight1 ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@freight1\",\"value\":10}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("le", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE orders[\\\"freight\\\"] <= @freight1 ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@freight1\",\"value\":10}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("gt", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE orders[\\\"freight\\\"] > @freight1 ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@freight1\",\"value\":3.67}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("ge", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE orders[\\\"freight\\\"] >= @freight1 ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@freight1\",\"value\":3.67}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("in", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE orders[\\\"shipCity\\\"] IN(@shipCity1, @shipCity2) ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCity1\",\"value\":\"Reims\"},{\"name\":\"@shipCity2\",\"value\":\"Charleroi\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("out", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE orders[\\\"shipCity\\\"] NOT IN(@shipCity1, @shipCity2) ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCity1\",\"value\":\"Reims\"},{\"name\":\"@shipCity2\",\"value\":\"Charleroi\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("and", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE orders[\\\"shipCity\\\"] = @shipCity1 AND orders[\\\"shipCountry\\\"] = @shipCountry2 ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCity1\",\"value\":\"Lyon\"},{\"name\":\"@shipCountry2\",\"value\":\"France\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("or", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE (orders[\\\"shipCity\\\"] = @shipCity1 OR orders[\\\"shipCity\\\"] = @shipCity2) ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCity1\",\"value\":\"Reims\"},{\"name\":\"@shipCity2\",\"value\":\"Charleroi\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("not", "SqlQuerySpec={\"query\":\"SELECT * FROM orders WHERE NOT ((orders[\\\"shipCity\\\"] = @shipCity1 OR orders[\\\"shipCity\\\"] = @shipCity2)) ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCity1\",\"value\":\"Reims\"},{\"name\":\"@shipCity2\",\"value\":\"Charleroi\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("as", "SqlQuerySpec={\"query\":\"SELECT *, orders[\\\"orderid\\\"] AS \\\"order_identifier\\\" FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("includes", "SqlQuerySpec={\"query\":\"SELECT orders[\\\"shipCountry\\\"], orders[\\\"shipCity\\\"], orders[\\\"orderId\\\"] FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("distinct", "SqlQuerySpec={\"query\":\"SELECT DISTINCT orders[\\\"shipCountry\\\"] FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("count1", "SqlQuerySpec={\"query\":\"SELECT COUNT(*) FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("count2", "SqlQuerySpec={\"query\":\"SELECT COUNT(@null1) FROM orders ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@null1\",\"value\":\"1\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("count3", "SqlQuerySpec={\"query\":\"SELECT COUNT(orders[\\\"shipRegion\\\"]) FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("countAs", "SqlQuerySpec={\"query\":\"SELECT COUNT(*) AS \\\"countOrders\\\" FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("sum", "SqlQuerySpec={\"query\":\"SELECT SUM(orders[\\\"freight\\\"]) FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("sumAs", "SqlQuerySpec={\"query\":\"SELECT SUM(orders[\\\"freight\\\"]) AS \\\"Sum Freight\\\" FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("sumIf", "SqlQuerySpec={\"query\":\"SELECT SUM(CASE WHEN orders[\\\"shipCountry\\\"] = @shipCountry1 THEN 1 ELSE 0 END) AS \\\"French Orders\\\" FROM orders ORDER BY orders[\\\"id\\\"] ASC\",\"parameters\":[{\"name\":\"@shipCountry1\",\"value\":\"France\"}]} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("min", "SqlQuerySpec={\"query\":\"SELECT MIN(orders[\\\"freight\\\"]) FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("max", "SqlQuerySpec={\"query\":\"SELECT MAX(orders[\\\"freight\\\"]) FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("groupCount", "SqlQuerySpec={\"query\":\"SELECT orders[\\\"shipCountry\\\"], COUNT(*) AS \\\"countryCount\\\" FROM orders GROUP BY orders[\\\"shipCountry\\\"] ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("offset", "SqlQuerySpec={\"query\":\"SELECT * FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("limit", "SqlQuerySpec={\"query\":\"SELECT * FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("page", "SqlQuerySpec={\"query\":\"SELECT * FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("pageNum", "SqlQuerySpec={\"query\":\"SELECT * FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("after", "SqlQuerySpec={\"query\":\"SELECT * FROM orders ORDER BY orders[\\\"id\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("sort", "SqlQuerySpec={\"query\":\"SELECT * FROM orders ORDER BY orders[\\\"shipCountry\\\"] DESC, orders[\\\"shipCity\\\"] ASC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-           //           .withResult("order", "SqlQuerySpec={\"query\":\"SELECT * FROM orders ORDER BY orders[\\\"shipCountry\\\"] ASC, orders[\\\"shipCity\\\"] DESC\"} FeedOptions={enableCrossPartitionQuery=true}")//
-
-           .withResult("ne", "UNSUPPORTED")//
-           .withResult("n", "UNSUPPORTED")//
-           .withResult("nn", "UNSUPPORTED")//
-           .withResult("emp", "UNSUPPORTED")//
-           .withResult("nemp", "UNSUPPORTED")//
-           .withResult("sw", "UNSUPPORTED")//
-           .withResult("ew", "UNSUPPORTED")//
-           .withResult("w", "UNSUPPORTED")//
-           .withResult("wo", "UNSUPPORTED")//
-           .withResult("lt", "UNSUPPORTED")//
-           .withResult("le", "UNSUPPORTED")//
-           .withResult("gt", "UNSUPPORTED")//
-           .withResult("ge", "UNSUPPORTED")//
-           .withResult("in", "UNSUPPORTED")//
-           .withResult("out", "UNSUPPORTED")//
-           .withResult("and", "UNSUPPORTED")//
-           .withResult("or", "UNSUPPORTED")//
+           .withResult("ne", "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"must_not\" : [ { \"term\" : { \"shipCountry\" : { \"value\" : \"France\", \"boost\" : 1.0 } } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("n", "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"must_not\" : [ { \"exists\" : { \"field\" : \"shipRegion\", \"boost\" : 1.0 } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("nn", "index: orders, QueryBuilder={ \"query\" : { \"exists\" : { \"field\" : \"shipRegion\", \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("emp", "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"should\" : [ { \"term\" : { \"shipRegion\" : { \"value\" : \"\", \"boost\" : 1.0 } } }, { \"bool\" : { \"must_not\" : [ { \"exists\" : { \"field\" : \"shipRegion\", \"boost\" : 1.0 } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("nemp",
+                 "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"must\" : [ { \"bool\" : { \"must_not\" : [ { \"term\" : { \"shipRegion\" : { \"value\" : \"\", \"boost\" : 1.0 } } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, { \"bool\" : { \"must\" : [ { \"exists\" : { \"field\" : \"shipRegion\", \"boost\" : 1.0 } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("sw", "index: orders, QueryBuilder={ \"query\" : { \"wildcard\" : { \"shipCountry\" : { \"wildcard\" : \"Franc*\", \"boost\" : 1.0 } } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("ew", "index: orders, QueryBuilder={ \"query\" : { \"wildcard\" : { \"shipCountry\" : { \"wildcard\" : \"*nce\", \"boost\" : 1.0 } } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("w", "index: orders, QueryBuilder={ \"query\" : { \"wildcard\" : { \"shipCountry\" : { \"wildcard\" : \"*ance*\", \"boost\" : 1.0 } } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("wo", "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"must_not\" : [ { \"wildcard\" : { \"shipCountry\" : { \"wildcard\" : \"*ance*\", \"boost\" : 1.0 } } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("lt", "index: orders, QueryBuilder={ \"query\" : { \"range\" : { \"freight\" : { \"from\" : null, \"to\" : \"10\", \"include_lower\" : true, \"include_upper\" : false, \"boost\" : 1.0 } } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("le", "index: orders, QueryBuilder={ \"query\" : { \"range\" : { \"freight\" : { \"from\" : null, \"to\" : \"10\", \"include_lower\" : true, \"include_upper\" : true, \"boost\" : 1.0 } } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("gt", "index: orders, QueryBuilder={ \"query\" : { \"range\" : { \"freight\" : { \"from\" : \"3.67\", \"to\" : null, \"include_lower\" : false, \"include_upper\" : true, \"boost\" : 1.0 } } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("ge", "index: orders, QueryBuilder={ \"query\" : { \"range\" : { \"freight\" : { \"from\" : \"3.67\", \"to\" : null, \"include_lower\" : true, \"include_upper\" : true, \"boost\" : 1.0 } } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("in", "index: orders, QueryBuilder={ \"query\" : { \"terms\" : { \"shipCity\" : [ \"Reims\", \"Charleroi\" ], \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("out", "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"must_not\" : [ { \"terms\" : { \"shipCity\" : [ \"Reims\", \"Charleroi\" ], \"boost\" : 1.0 } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("and", "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"filter\" : [ { \"term\" : { \"shipCity\" : { \"value\" : \"Lyon\", \"boost\" : 1.0 } } }, { \"term\" : { \"shipCountry\" : { \"value\" : \"France\", \"boost\" : 1.0 } } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("or", "index: orders, QueryBuilder={ \"query\" : { \"bool\" : { \"should\" : [ { \"term\" : { \"shipCity\" : { \"value\" : \"Reims\", \"boost\" : 1.0 } } }, { \"term\" : { \"shipCity\" : { \"value\" : \"Charleroi\", \"boost\" : 1.0 } } } ], \"adjust_pure_negative\" : true, \"boost\" : 1.0 } }, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
            .withResult("not", "UNSUPPORTED")//
            .withResult("as", "UNSUPPORTED")//
            .withResult("includes", "UNSUPPORTED")//
@@ -105,12 +67,11 @@ public class ElasticRqlUnitTest extends AbstractRqlTest
            .withResult("groupCount", "UNSUPPORTED")//
            .withResult("offset", "UNSUPPORTED")//
            .withResult("limit", "UNSUPPORTED")//
-           .withResult("page", "UNSUPPORTED")//
-           .withResult("pageNum", "UNSUPPORTED")//
+           .withResult("page", "index: orders, QueryBuilder={ \"from\" : 3, \"size\" : 7, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("pageNum", "index: orders, QueryBuilder={ \"from\" : 3, \"size\" : 7, \"sort\" : [ { \"id\" : { \"order\" : \"asc\" } } ] }")//
            .withResult("after", "UNSUPPORTED")//
-           .withResult("sort", "UNSUPPORTED")//
-           .withResult("order", "UNSUPPORTED")//
-
+           .withResult("sort", "index: orders, QueryBuilder={ \"sort\" : [ { \"shipCountry\" : { \"order\" : \"desc\" } }, { \"shipCity\" : { \"order\" : \"asc\" } }, { \"id\" : { \"order\" : \"asc\" } } ] }")//
+           .withResult("order", "index: orders, QueryBuilder={ \"sort\" : [ { \"shipCountry\" : { \"order\" : \"asc\" } }, { \"shipCity\" : { \"order\" : \"desc\" } }, { \"id\" : { \"order\" : \"asc\" } } ] }")//
            .withResult("likeMiddle", "UNSUPPORTED")//
            .withResult("likeStartsWith", "UNSUPPORTED")//
            .withResult("likeEndsWith", "UNSUPPORTED")//
